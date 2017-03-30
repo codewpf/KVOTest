4462f62065c4f93b314529d68b46e4e5e7700305
ioswpf <ioswpf@gmail.com>
2017-03-30 17:58:34 +0800

    Initial Commit

---
+++ KVOTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVOTest
+//
+//  Created by wpf on 2017/3/30.
+//  Copyright © 2017年 wpf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVOTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVOTest
+//
+//  Created by wpf on 2017/3/30.
+//  Copyright © 2017年 wpf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
